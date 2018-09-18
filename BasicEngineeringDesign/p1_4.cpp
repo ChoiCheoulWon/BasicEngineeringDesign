d25c3cde03d2c00088b4ab3e2c36ef9434b667c2
DESKTOP-P6EPFHS\choicheoulwon <choicheoulwon@gmail.com>
2018-09-19 00:33:28 +0900

    p1_4.cpp추가
    몫과 나머지를 구하는 프로그램

---
+++ BasicEngineeringDesign/p1_4.cpp
@@ -0,0 +1,14 @@
+//몫과 나머지 구하는 프로그램
+
+#include<stdio.h>
+
+int main() {
+	int num1, num2;
+
+	printf("Enter two integral numbers: ");
+	scanf("%d %d", &num1, &num2);
+
+	printf("%d / %d is %d with a remainder of : %d\n", num1, num2, num1 / num2, num1%num2);
+
+	return 0;
+}
