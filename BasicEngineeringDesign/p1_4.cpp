26a0f5d0c06aa4257c73cfeb4babeb826e15eb90
DESKTOP-P6EPFHS\choicheoulwon <choicheoulwon@gmail.com>
2018-09-19 00:43:26 +0900

    p1_4.cpp 코드 수정
    코드 수정

--- BasicEngineeringDesign/p1_4.cpp
+++ BasicEngineeringDesign/p1_4.cpp
@@ -8,7 +8,9 @@ int main() {
 	printf("Enter two integral numbers: ");
 	scanf("%d %d", &num1, &num2);
 
+
 	printf("%d / %d is %d with a remainder of : %d\n", num1, num2, num1 / num2, num1%num2);
+
 
 	return 0;
 }
